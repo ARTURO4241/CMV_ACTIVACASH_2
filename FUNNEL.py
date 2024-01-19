064ebf6893f60385efcca5dd8d51d91de7b8b69b
ARTURO4241 <157048515+ARTURO4241@users.noreply.github.com>
2024-01-19 09:18:18 -0600

    Update FUNNEL.py

--- FUNNEL.py
+++ FUNNEL.py
@@ -11,22 +11,25 @@ st.title("PROTOTIPO FUNNELL")
 #df_2=pd.read_csv('APROBADOS.csv')
 #st.write(df_2)
 
-st.markdown(f':FUNNEL')
+st.title(f':FUNNEL')
 st.image('FUNNEL.png',caption='ACTIVIDAD REGISTRADA PARA CADA UNA DE LAS PANTALLAS')
 
 
-st.markdown(f':TRAFICO TOTAL')
+st.title(f': AFLUENCIA DIARIA EN EL UNBORDING 2.0')
 st.image('BARRAS.png',caption='EVOLUCION DE LA ACTIVIDAD')
 
 
-st.markdown(f':CAIDOS')
+st.title(f':CAIDOS')
 df=pd.read_csv('EN PROCESO.csv',encoding='latin-1')
+df=pd.drop('Unnamed:0',axis=1)
 st.write(df)
 
-st.markdown(f':APROBADOS')
+st.title(f':APROBADOS')
 df=pd.read_csv('APROBADOS.csv',encoding='latin-1')
+df=pd.drop('Unnamed:0',axis=1)
 st.write(df)
 
-st.markdown(f':RECHAZADOS')
+st.title(f':RECHAZADOS')
 df=pd.read_csv('RECHAZADO.csv',encoding='latin-1')
+df=pd.drop('Unnamed:0',axis=1)
 st.write(df)
