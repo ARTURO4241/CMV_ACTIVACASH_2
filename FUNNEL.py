a923915c43223b1f047184becc755e1768537586
ARTURO4241 <157048515+ARTURO4241@users.noreply.github.com>
2024-01-19 10:06:46 -0600

    Update FUNNEL.py

--- FUNNEL.py
+++ FUNNEL.py
@@ -16,24 +16,24 @@ st.title("PROTOTIPO FUNNELL")
 #df_2=pd.read_csv('APROBADOS.csv')
 #st.write(df_2)
 
-st.title(f':FUNNEL')
+st.title(f'FUNNEL')
 
-st.markdown(f':neutral_face: FUNNEL')
+st.markdown(f' FUNNEL')
 st.image('FUNNEL.png',caption='ACTIVIDAD REGISTRADA PARA CADA UNA DE LAS PANTALLAS')
 
 
-st.markdown(f':neutral_face: AFLUENCIA DIARIA EN EL UNBORDING 2.0')
+st.markdown(f' AFLUENCIA DIARIA EN EL UNBORDING 2.0')
 st.image('BARRAS.png',caption='EVOLUCION DE LA ACTIVIDAD')
 
 
-st.markdown(f':neutral_face: CAIDOS')
+st.markdown(f' CAIDOS')
 df=pd.read_csv('EN PROCESO.csv',encoding='latin-1')
 df=df.drop('Unnamed: 0',axis=1)
 df['NUMERO_SOCIO']=list(map(reemplazos,df['NUMERO_SOCIO']))
 df['CELULAR']=list(map(reemplazos,df['CELULAR']))
 st.write(df)
 
-st.markdown(f':neutral_face: APROBADOS')
+st.markdown(f' APROBADOS')
 df=pd.read_csv('APROBADOS.csv',encoding='latin-1')
 df=df.drop('Unnamed: 0',axis=1)
 df=df.drop('PASO',axis=1)
@@ -41,7 +41,7 @@ df['NUMERO_SOCIO']=list(map(reemplazos,df['NUMERO_SOCIO']))
 df['CELULAR']=list(map(reemplazos,df['CELULAR']))
 st.write(df)
 
-st.markdown(f':neutral_face: RECHAZADOS')
+st.markdown(f' RECHAZADOS')
 df=pd.read_csv('RECHAZADO.csv',encoding='latin-1')
 df=df.drop('Unnamed: 0',axis=1)
 df['NUMERO_SOCIO']=list(map(reemplazos,df['NUMERO_SOCIO']))
