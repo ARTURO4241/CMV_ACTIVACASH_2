8ade4a27445184567afeab61b00d50c092fa3994
ARTURO4241 <157048515+ARTURO4241@users.noreply.github.com>
2024-01-22 10:42:49 -0600

    Update FUNNEL.py

--- FUNNEL.py
+++ FUNNEL.py
@@ -1,7 +1,8 @@
 from datetime import datetime, timedelta
 import streamlit as st
 import pandas as pd
-#import plotly.express as px
+import numpy as np
+import pydeck as pdk
 import pip 
 
 def reemplazos(v):
@@ -46,19 +47,57 @@ df=pd.read_csv('EN PROCESO.csv',encoding='latin-1')
 df=df.drop('Unnamed: 0',axis=1)
 df['NUMERO_SOCIO']=list(map(reemplazos,df['NUMERO_SOCIO']))
 df['CELULAR']=list(map(reemplazos,df['CELULAR']))
+COOREDENADAS_CAIDOS=df[['LATITUD','LONGITUD']]
+df=df.drop('LATITUD',axis=1)
+df=df.drop('LONGITUD',axis=1)
 st.write(df)
 
-st.markdown(f' APROBADOS')
-df=pd.read_csv('APROBADOS.csv',encoding='latin-1')
-df=df.drop('Unnamed: 0',axis=1)
-df=df.drop('PASO',axis=1)
-df['NUMERO_SOCIO']=list(map(reemplazos,df['NUMERO_SOCIO']))
-df['CELULAR']=list(map(reemplazos,df['CELULAR']))
-st.write(df)
 
 st.markdown(f' RECHAZADOS')
 df=pd.read_csv('RECHAZADO.csv',encoding='latin-1')
 df=df.drop('Unnamed: 0',axis=1)
 df['NUMERO_SOCIO']=list(map(reemplazos,df['NUMERO_SOCIO']))
 df['CELULAR']=list(map(reemplazos,df['CELULAR']))
+COOREDENADAS_RECHAZO=df[['LATITUD','LONGITUD']]
+df=df.drop('LATITUD',axis=1)
+df=df.drop('LONGITUD',axis=1)
 st.write(df)
+
+COORDENADAS_CAIDOS=COORDENADAS_CAIDOS.dropna()
+COOREDENADAS_RECHAZO=COOREDENADAS_RECHAZO.dropna()
+
+
+ 
+chart_data = pd.DataFrame(
+   np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+   columns=['lat', 'lon'])
+ 
+st.pydeck_chart(pdk.Deck(
+    map_style=None,
+    initial_view_state=pdk.ViewState(
+        latitude=37.76,
+        longitude=-122.4,
+        zoom=11,
+        pitch=50,
+    ),
+    layers=[
+        pdk.Layer(
+           'HexagonLayer',
+           data=chart_data,
+           get_position='[lon, lat]',
+           radius=200,
+           elevation_scale=4,
+           elevation_range=[0, 1000],
+           pickable=True,
+           extruded=True,
+        ),
+        pdk.Layer(
+            'ScatterplotLayer',
+            data=chart_data,
+            get_position='[lon, lat]',
+            get_color='[200, 30, 0, 160]',
+            get_radius=200,
+        ),
+    ],
+))
+
