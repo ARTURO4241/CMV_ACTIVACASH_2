94edd3ff068bfb598087f8bd15f5602940b2ff96
ARTURO4241 <157048515+ARTURO4241@users.noreply.github.com>
2024-01-22 09:44:01 -0600

    Update FUNNEL.py

--- FUNNEL.py
+++ FUNNEL.py
@@ -11,6 +11,7 @@ def reemplazos(v):
 pip.main(["install","openpyxl"])
 
 st.title("PROTOTIPO FUNNELL")
+st.text('La presente página permite visualizar el comportamiento generalizado de los socios en la aplicacion.')
 #df=pd.read_excel('PROCESOS_PIVOTE.xlsx')
 
 #df_2=pd.read_csv('APROBADOS.csv')
@@ -23,6 +24,11 @@ st.image('FUNNEL.png',caption='ACTIVIDAD REGISTRADA PARA CADA UNA DE LAS PANTALL
 st.markdown(f' AFLUENCIA DIARIA EN EL UNBORDING 2.0')
 st.image('BARRAS.png',caption='EVOLUCION DE LA ACTIVIDAD')
 
+st.markdown(f' RESUMEN')
+df=pd.read_csv('RESUMEN.csv',encoding='latin-1')
+df=df.drop('Unnamed: 0',axis=1)
+st.text('Con la finalidad de presentar de manera rápida los resultados reelevantes acerca del uso de la app, se muestra a continuación una tabla de concentración.')
+st.write(df)
 
 st.markdown(f' CAIDOS')
 df=pd.read_csv('EN PROCESO.csv',encoding='latin-1')
