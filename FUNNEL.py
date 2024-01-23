94bdb793e8c3ea6ebecba0791ca82b9fb1d03302
ARTURO4241 <157048515+ARTURO4241@users.noreply.github.com>
2024-01-23 09:15:41 -0600

    Update FUNNEL.py

--- FUNNEL.py
+++ FUNNEL.py
@@ -31,6 +31,11 @@ data = dict(
 #st.ploty_chart(fig)
 #fig.show()
 st.image('FUNNEL.png',caption='ACTIVIDAD REGISTRADA PARA CADA UNA DE LAS PANTALLAS')
+data = dict(
+    number=[39, 27.4, 20.6, 11, 2],
+    stage=["Website visit", "Downloads", "Potential customers", "Requested price", "invoice sent"])
+fig = px.funnel(data, x='number', y='stage')
+fig.show()
 
 
 st.markdown(f' AFLUENCIA DIARIA EN EL UNBORDING 2.0')
