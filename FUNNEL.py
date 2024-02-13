7a0b8ef85fc047eb88be2d04682575462f1cdf9c
ARTURO4241 <157048515+ARTURO4241@users.noreply.github.com>
2024-02-13 17:11:01 -0600

    Update FUNNEL.py

--- FUNNEL.py
+++ FUNNEL.py
@@ -86,11 +86,11 @@ st.pydeck_chart(pdk.Deck(
 #st.plotly_chart(fig)
 
 
-st.markdown(f' ABANDONARON EN HOME O DATOS PERSONALES')
-df=pd.read_csv('POR EVALUAR.csv',encoding='latin-1')
-df=df.drop('Unnamed: 0',axis=1)
-df['CELULAR']=list(map(reemplazos,df['CELULAR']))
-st.write(df)
+#st.markdown(f' ABANDONARON EN HOME O DATOS PERSONALES')
+#df=pd.read_csv('POR EVALUAR.csv',encoding='latin-1')
+#df=df.drop('Unnamed: 0',axis=1)
+#df['CELULAR']=list(map(reemplazos,df['CELULAR']))
+#st.write(df)
 
 
 st.markdown(f' ABANDONARON DURANTE EL PROCESO')
